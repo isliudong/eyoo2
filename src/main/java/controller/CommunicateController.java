57cdf3c8b159a008e57f86843cff8608378a8dea
microwangwei <15198159758@163.com>
2019-12-03 23:24:35 +0800

    聊天功能类开始编写

--- src/main/java/controller/CommunicateController.java
+++ src/main/java/controller/CommunicateController.java
@@ -2,5 +2,13 @@ package controller;
 
 public class CommunicateController {
     //对项目的聊天室进行编写
-    
+    private int a;
+    private int b;
+    private int sum;
+    public void CommunicateController(int a,int b)
+    {
+        sum=a+b;
+    }
+    //a发送一个请求到服务端，服务端响应然后把请求的内容发给林一个人，
+    // 这样就实现了聊天的功能
 }
