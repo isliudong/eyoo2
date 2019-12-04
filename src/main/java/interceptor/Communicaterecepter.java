93c74235d907e814f15c5d08ed641182102b6c92
microwangwei <15198159758@163.com>
2019-12-04 12:41:57 +0800

    完成Communicate功能的编写

--- src/main/java/interceptor/Communicaterecepter.java
+++ src/main/java/interceptor/Communicaterecepter.java
@@ -10,11 +10,26 @@ public class Communicaterecepter extends LoginInterceptor {
         if (receptor=="admin")
         {
             System.out.println("Recive the misiion");
+            receptor="success";
         }
         else
         {
             System.out.println("Reception is erro!");
+            receptor="faile";
         }
         //发送到远程服务端
+        //完成服务端的请求并返回
     }
+    public void Reback(String receptor)
+    {
+        if(receptor == "success")
+        {
+            System.out.println("请求成功");
+        }
+        else
+        {
+            System.out.println("请求失败");
+        }
+    }
+
 }
