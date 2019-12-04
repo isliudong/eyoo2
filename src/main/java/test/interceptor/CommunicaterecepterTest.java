2776f903f88c5cc10c18300e4399f07551ba5842
microwangwei <15198159758@163.com>
2019-12-04 13:02:43 +0800

    对communicate功能进行单元测试

---
+++ src/main/java/test/interceptor/CommunicaterecepterTest.java
@@ -0,0 +1,35 @@
+package test.interceptor; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* Communicaterecepter Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Ê®¶þÔÂ 4, 2019</pre> 
+* @version 1.0 
+*/ 
+public class CommunicaterecepterTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: Reback(String receptor) 
+* 
+*/ 
+@Test
+public void testReback() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
